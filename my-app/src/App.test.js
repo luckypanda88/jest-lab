66119d3ca4eecfbec933d36d574030ce437887b6
luckypanda88 <brendah8@uw.edu>
2023-07-20 18:05:15 -0700

    added 2 tests: class and own example

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,23 @@
+//Brenda 
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
 
-test('renders learn react link', () => {
+test('Class Example: Test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  //Event: Click on the checkbox
+  userEvent.click(screen.getByTestId("checkbox"));
+  
+  //Checking if checkbox is not checked.
+  expect(screen.getByTestId("checkbox")).toBeChecked()
+
+  //screen.debug();
+  
 });
+
+// Brenda's Individual Test:
+test("testing for This checkbox is now set to false", () => {
+  render(<App />);
+  expect(screen.getByText("This checkbox is now set to false")).toBeInTheDocument();
+})
